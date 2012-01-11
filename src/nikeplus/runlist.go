495c4d85a43313216abb11fe21923b958820d145
Andy Gregorowicz <andy@gregorowicz.org>
2012-01-10 20:09:38 -0500

    Parsing the whole plusService xml document

--- src/nikeplus/runlist.go
+++ src/nikeplus/runlist.go
@@ -1,17 +1,30 @@
 package nikeplus
 
-import (
-  "xml"
-)
-
 type Run struct {
   Id int `xml:"attr"`
   StartTime string
-  Distance float32
+  Distance float64
   Duration int
 }
 
 type RunList struct {
-  XMLName xml.Name `xml:"runList"`
   Run []Run
+}
+
+type RunListSummary struct {
+  Runs int
+  Distance float64
+  RunDuration int
+  Calories float64
+  Duration int
+}
+
+func (rls RunListSummary) DistanceInMiles() float64 {
+  return rls.Distance * 0.621371192
+}
+
+type PlusService struct {
+  RunList RunList
+  RunListSummary RunListSummary
+  Status string
 }
--- src/nikeplus/runlist_test.go
+++ src/nikeplus/runlist_test.go
@@ -9,16 +9,25 @@ import (
 func TestLoadXml(t *testing.T) {
   file, err := os.Open("../../fixtures/example.xml")
   if err == nil {
-    rl := RunList{Run: nil}
-    xml.Unmarshal(file, &rl)
+    ps := new(PlusService)
+    xml.Unmarshal(file, ps)
     file.Close()
     
-    firstRun := &rl.Run[0]
+    firstRun := &ps.RunList.Run[0]
     expectedId := 1484663528
     if firstRun.Id != expectedId {
       t.Errorf("Expected %d but got %d", expectedId, firstRun.Id)
     }
   } else {
     t.Error(err)
+  }
+}
+
+func TestDistanceInMiles(t *testing.T) {
+  rls := new(RunListSummary)
+  rls.Distance = 15
+  expectedDistance := 9.32056788
+  if rls.DistanceInMiles() != expectedDistance {
+    t.Errorf("Expected %d but got %d", expectedDistance, rls.DistanceInMiles())
   }
 }
