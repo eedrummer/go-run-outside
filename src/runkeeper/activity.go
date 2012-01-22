394e43cd789a5797658244885478b2e607dc233e
Andy Gregorowicz <andy@gregorowicz.org>
2012-01-21 22:29:36 -0500

    Starting work on the runkeeper pacakge

---
+++ src/runkeeper/activity.go
@@ -0,0 +1,15 @@
+package runkeeper
+
+type Distance struct {
+  Timestamp int `json:"timestamp"`
+  Distance float64 `json:"distance"`
+}
+
+type Activity struct {
+  Type string `json:"type"`
+  StartTime string `json:"start_time"`
+  TotalDistance float64 `json:"total_distance"`
+  Distance []Distance `json:"distance"`
+  Duration int `json:"duration"`
+  TotalCalories float64 `json:"total_calories"`
+}
---
+++ src/runkeeper/activity_test.go
@@ -0,0 +1,13 @@
+package runkeeper
+
+import (
+  "testing"
+  "json"
+  "fmt"
+)
+
+func TestJSONMarshalling(t *testing.T) {
+  activity := Activity{"Running", "Sat, 1 Jan 2011 00:00:00", 3.4, nil, 1000, 355.0}
+  outputJSON, _ := json.Marshal(activity)
+  fmt.Printf("JSON Output: %s", outputJSON)
+}
