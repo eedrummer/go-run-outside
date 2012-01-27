c0513b909439e99bb6f15ea81524d0455430c95e
Andy Gregorowicz <andy@gregorowicz.org>
2012-01-26 22:19:05 -0500

    Added function to convert NikePlus info into RunKeeper info

--- conversion.go
+++ conversion.go
@@ -3,14 +3,32 @@ package main
 import (
   "strings"
   "strconv"
+  "time"
+  "fmt"
 )
 
-func ConvertExtendedData(extendedData string) *[]Distance {
+func ConvertExtendedData(extendedData string) []Distance {
   kmStrings := strings.Split(extendedData, ", ")
   distances := make([]Distance, len(kmStrings))
   for i := 0; i < len(kmStrings); i++ {
     kmDistance, _ := strconv.Atof64(kmStrings[i])
     distances[i] = Distance{i * 10, kmDistance * 1000}
   }
-  return &distances
+  return distances
+}
+
+func ConvertNikePlusToRunKeeper(plusService *PlusService) *Activity {
+  activity := new(Activity)
+  activity.Type = "Running"
+  t := plusService.SportsData.ParsedStartTime()
+  if t != nil {
+    activity.StartTime = t.Format(time.RFC1123)
+  } else {
+    fmt.Print("Couldn't find a start time for run")
+  }
+  activity.TotalDistance = plusService.SportsData.RunSummary.Distance * 1000
+  activity.Distance = ConvertExtendedData(plusService.SportsData.ExtendedDataList.ExtendedData)
+  activity.Duration = plusService.SportsData.RunSummary.Duration
+  activity.TotalCalories = plusService.SportsData.RunSummary.Calories
+  return activity
 }
--- conversion_test.go
+++ conversion_test.go
@@ -3,10 +3,13 @@ package main
 import (
   "testing"
   "github.com/bmizerany/assert"
+  "os"
+  "xml"
+  "math"
 )
 
 func TestConvertExtendedData(t *testing.T) {
-  distances := *ConvertExtendedData("0.0, 0.0182, 0.0456, 0.0703")
+  distances := ConvertExtendedData("0.0, 0.0182, 0.0456, 0.0703")
   assert.Equal(t, 4, len(distances))
   assert.Equal(t, 0, distances[0].Timestamp)
   assert.Equal(t, 0.0, distances[0].Distance)
@@ -14,4 +17,24 @@ func TestConvertExtendedData(t *testing.T) {
   assert.Equal(t, 18.2, distances[1].Distance)
   assert.Equal(t, 20, distances[2].Timestamp)
   assert.Equal(t, 45.6, distances[2].Distance)
+}
+
+func TestConvertNikePlusToRunKeeper(t *testing.T) {
+  file, err := os.Open("fixtures/individual_run.xml")
+  if err == nil {
+    ps := new(PlusService)
+    xml.Unmarshal(file, ps)
+    file.Close()
+    
+    activity := ConvertNikePlusToRunKeeper(ps)
+    
+    assert.Equal(t, "Running", activity.Type)
+    assert.Equal(t, "Sun, 09 Dec 2011 07:11:45 -0500", activity.StartTime)
+    assert.Equal(t, 3951.9, activity.TotalDistance)
+    assert.Equal(t, 160, len(activity.Distance))
+    assert.Equal(t, 18.0, math.Floor(activity.Distance[1].Distance))
+    assert.Equal(t, 3925.0, math.Floor(activity.Distance[159].Distance))
+  } else {
+    t.Error(err)
+  }
 }
